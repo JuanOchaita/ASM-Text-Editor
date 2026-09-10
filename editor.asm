f38f441b1ab9d4bd3fcfae23924e2f66f29b3388
Kris Paz <krispaz77@gmail.com>
2026-09-10 11:06:41 -0600

    borrar el carácter y más

--- editor.asm
+++ editor.asm
@@ -1,14 +1,14 @@
 ; Proyecto 1 - Arquitectura y Diseno de Computadoras
-; Primera entrega de la pantalla de edicion (x8086 / MASM-TASM)
+; Avance aproximado: 60% de la pantalla de edicion (x8086 / MASM-TASM)
 ;
-; Implementado en esta parte:
-;   - Marco visual de la pantalla de edicion.
-;   - Escritura de letras, numeros, espacio, coma, punto y dos puntos.
-;   - Movimiento del cursor con las cuatro flechas, limitado al area de texto.
-;   - ESC vuelve al programa que llamo al editor.
+; Implementado:
+;   - Marco visual y area editable de 80x25.
+;   - Escritura: letras, numeros, espacio, coma, punto y dos puntos.
+;   - Flechas y Backspace, limitados al area de texto.
+;   - Alt+C (centrar), Alt+U (primer renglon) y Alt+D (ultimo renglon).
+;   - ESC retorna al procedimiento que llama al editor.
 ;
-; Pendiente para las siguientes partes: archivos, ALT+S, colores, imagenes,
-; buscar/reemplazar y la pantalla de ayuda.
+; Pendiente: archivos, ALT+S, colores, imagenes, buscar/reemplazar y ayuda.
 
 .model small
 .stack 100h
@@ -20,9 +20,8 @@ TEXT_ATTR   EQU 0Fh
 .data
 titleLine   db '  EDITOR DE TEXTO  |  Documento sin guardar', 0
 borderLine  db '--------------------------------------------------------------------------------', 0
-hintLine    db 'Flechas: mover  |  A-Z, 0-9, espacio, , . :  |  ESC: volver', 0
+hintLine    db 'Flechas: mover | Alt+C: centro | Alt+U/D: arriba/abajo | ESC: volver', 0
 statusLine  db 'Estado: EDITANDO', 0
-
 cursorRow   db EDIT_TOP
 cursorCol   db 0
 
@@ -30,14 +29,12 @@ cursorCol   db 0
 main PROC
     mov ax, @data
     mov ds, ax
-
     call PantallaEdicion
-
     mov ax, 4C00h
     int 21h
 main ENDP
 
-; Dibuja la interfaz y conserva el control hasta que se presione ESC.
+; Se puede llamar desde el menu principal en vez de usar main directamente.
 PantallaEdicion PROC NEAR
     mov ax, 0003h                 ; modo texto 80x25 y limpiar pantalla
     int 10h
@@ -47,25 +44,21 @@ PantallaEdicion PROC NEAR
     mov si, OFFSET titleLine
     mov bl, 1Fh
     call ImprimirCadena
-
     mov dh, 1
     mov dl, 0
     mov si, OFFSET borderLine
     mov bl, 1Bh
     call ImprimirCadena
-
     mov dh, 22
     mov dl, 0
     mov si, OFFSET borderLine
     mov bl, 1Bh
     call ImprimirCadena
-
     mov dh, 23
     mov dl, 1
     mov si, OFFSET hintLine
     mov bl, 0Ah
     call ImprimirCadena
-
     mov dh, 24
     mov dl, 1
     mov si, OFFSET statusLine
@@ -79,19 +72,25 @@ LeerTecla:
     call ColocarCursor
     mov ah, 00h
     int 16h
-
-    cmp al, 27                    ; ESC: regresar al menu principal al integrarlo
+    cmp al, 27                    ; ESC
     je  FinEditor
-
+    cmp al, 8                     ; Backspace
+    je  BorrarAnterior
     cmp al, 0
     je  TeclaExtendida
-
     call EsCaracterPermitido
-    jc  LeerTecla                 ; no se dibuja una tecla no permitida
+    jc  LeerTecla
     call EscribirCaracter
     jmp LeerTecla
 
 TeclaExtendida:
+    ; Alt + letra se recibe como AL=0 y AH=el scan code de la letra.
+    cmp ah, 2Eh                   ; Alt+C
+    je  CentrarCursor
+    cmp ah, 16h                   ; Alt+U
+    je  IrArriba
+    cmp ah, 20h                   ; Alt+D
+    je  IrAbajo
     cmp ah, 48h                   ; flecha arriba
     je  Subir
     cmp ah, 50h                   ; flecha abajo
@@ -107,13 +106,11 @@ Subir:
     je  LeerTecla
     dec cursorRow
     jmp LeerTecla
-
 Bajar:
     cmp cursorRow, EDIT_BOTTOM
     je  LeerTecla
     inc cursorRow
     jmp LeerTecla
-
 Izquierda:
     cmp cursorCol, 0
     jne MoverIzquierda
@@ -125,7 +122,6 @@ Izquierda:
 MoverIzquierda:
     dec cursorCol
     jmp LeerTecla
-
 Derecha:
     cmp cursorCol, 79
     jne MoverDerecha
@@ -138,11 +134,42 @@ MoverDerecha:
     inc cursorCol
     jmp LeerTecla
 
+CentrarCursor:
+    mov cursorCol, 40
+    jmp LeerTecla
+IrArriba:
+    mov cursorRow, EDIT_TOP
+    jmp LeerTecla
+IrAbajo:
+    mov cursorRow, EDIT_BOTTOM
+    jmp LeerTecla
+
+; Retrocede una posicion, la limpia y conserva el cursor en ella.
+BorrarAnterior:
+    cmp cursorCol, 0
+    jne RetrocederColumna
+    cmp cursorRow, EDIT_TOP
+    je  LeerTecla
+    dec cursorRow
+    mov cursorCol, 79
+    jmp PintarEspacio
+RetrocederColumna:
+    dec cursorCol
+PintarEspacio:
+    call ColocarCursor
+    mov al, ' '
+    mov ah, 09h
+    mov bh, 0
+    mov bl, TEXT_ATTR
+    mov cx, 1
+    int 10h
+    jmp LeerTecla
+
 FinEditor:
     ret
 PantallaEdicion ENDP
 
-; Entrada: AL = caracter. Salida: CF=0 si es admitido; CF=1 si no lo es.
+; Entrada AL. CF=0 si el caracter se puede escribir; CF=1 si no.
 EsCaracterPermitido PROC NEAR
     cmp al, 'A'
     jb  RevisarMinuscula
@@ -174,25 +201,22 @@ Permitido:
     ret
 EsCaracterPermitido ENDP
 
-; Escribe AL con el atributo actual y avanza una posicion sin salir del area.
+; Escribe AL y avanza sin salir del area editable.
 EscribirCaracter PROC NEAR
     push ax
     push bx
     push cx
     push dx
-
     call ColocarCursor
     mov ah, 09h
     mov bh, 0
     mov bl, TEXT_ATTR
     mov cx, 1
     int 10h
-
     pop dx
     pop cx
     pop bx
     pop ax
-
     cmp cursorCol, 79
     jne AvanzarColumna
     cmp cursorRow, EDIT_BOTTOM
@@ -206,7 +230,6 @@ FinEscritura:
     ret
 EscribirCaracter ENDP
 
-; Lleva el cursor fisico a cursorRow, cursorCol.
 ColocarCursor PROC NEAR
     mov ah, 02h
     mov bh, 0
@@ -216,7 +239,7 @@ ColocarCursor PROC NEAR
     ret
 ColocarCursor ENDP
 
-; Entrada: DH=fila, DL=columna, DS:SI=texto terminado en 0, BL=atributo.
+; Entrada: DH=fila, DL=columna, DS:SI=cadena terminada en 0, BL=atributo.
 ImprimirCadena PROC NEAR
     push ax
     push bx
@@ -226,7 +249,6 @@ ImprimirCadena PROC NEAR
     push di
     push es
     push si
-
     mov di, si
     xor cx, cx
 ContarCaracter:
@@ -242,7 +264,6 @@ Imprimir:
     mov ax, 1301h
     mov bh, 0
     int 10h
-
     pop si
     pop es
     pop di
